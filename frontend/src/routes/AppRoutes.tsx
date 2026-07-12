699ea9667dde6473a00c88809b7e76a609c3daf7
pitical87 <pitical87@gmail.com>
2026-07-12 19:23:52 +0900

    feat : implement guesroute and protectedroute

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,13 +1,21 @@
 import { Routes, Route } from "react-router";
-
 import ClientPage from "../pages/ClientPage";
 import PresentPage from "../pages/PresentPage";
+import LoginPage from "../pages/LoginPage";
+import GuestRoute from "../components/GuestRoute";
+import ProtectedRoutes from "../components/ProtectedRoutes";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<ClientPage />} />
-      <Route path="/present/:type" element={<PresentPage />} />
+      <Route element={<GuestRoute />}>
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
+
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/" element={<ClientPage />} />
+        <Route path="/present/:type" element={<PresentPage />} />
+      </Route>
     </Routes>
   );
 }
