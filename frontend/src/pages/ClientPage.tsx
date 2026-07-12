d99b5a71976cb4a9f594be15e93455f6ab540502
pitical87 <pitical87@gmail.com>
2026-07-12 19:22:55 +0900

    refact : take attendence data from api (not local storage) and add TopNavbar & SifSection

--- frontend/src/pages/ClientPage.tsx
+++ frontend/src/pages/ClientPage.tsx
@@ -2,22 +2,38 @@ import HeroCard from "../components/Client Page/HeroCard";
 import Absensi from "../components/Client Page/Absensi";
 import PresentStatistic from "../components/Client Page/PresentStatistic";
 import RecentPresents from "../components/Client Page/RecentPresents";
-import { GetCurrentDateString } from "../utils/DateUtils";
-import { getAttendance } from "../utils/Storage";
+import TopNavbar from "../components/Client Page/TopNavbar";
+import SifSection from "../components/Client Page/SifSection";
+import { useEffect, useState } from "react";
+import { getStatus } from "../utils/api/Attendence";
 
 export default function ClientPage() {
-  const today = GetCurrentDateString();
-  const attendence = getAttendance();
-  const todayRecords = attendence.filter((r) => r.date === today);
-  const masuk = todayRecords.find((r) => r.type === "masuk");
-  const pulang = todayRecords.find((r) => r.type === "pulang");
+  const [hasMasuk, setHasMasuk] = useState(false);
+  const [hasPulang, setHasPulang] = useState(false);
+  const [masuk, setMasuk] = useState<{ waktu: string; status: string } | null>(
+    null,
+  );
+  const [pulang, setPulang] = useState<{ waktu: string } | null>(null);
 
-  const hasMasuk: boolean = masuk !== null;
-  const hasPulang: boolean = pulang !== null;
+  useEffect(() => {
+    getStatus()
+      .then((res) => {
+        if (res.sukses) {
+          setHasMasuk(!!res.absen_masuk);
+          setHasPulang(!!res.absen_pulang);
+          setMasuk(res.absen_masuk);
+          setPulang(res.absen_pulang);
+        }
+      })
+      .catch(() => {});
+  }, []);
   return (
     <div>
+      <TopNavbar />
       {/* second top part */}
       <HeroCard masuk={masuk} pulang={pulang} />
+      {/* sif & absensi section */}
+      <SifSection />
       {/* present section */}
       <Absensi hasMasuk={hasMasuk} hasPulang={hasPulang} />
       {/* statistic section */}
