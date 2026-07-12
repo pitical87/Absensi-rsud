a2d370310496f9f9b3d30536714637dcff5af1cf
pitical87 <pitical87@gmail.com>
2026-07-12 19:18:18 +0900

    remove TopNavbar to clientPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
-import TopNavbar from "./components/Client Page/TopNavbar";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <div className="min-h-screen bg-[#f1f5f9] flex justify-center">
       <div className="w-full md:w-md ">
-        {/* top part */}
-        <TopNavbar />
         <AppRoutes />
       </div>
     </div>
