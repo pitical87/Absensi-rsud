710601995cfb86aff52aff7212838900f536b0ad
pitical87 <pitical87@gmail.com>
2026-07-21 18:40:53 +0900

    feat : add taost and react hook library

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,13 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { AuthProvider } from "./context/AuthContext.tsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <AuthProvider>
       <App />
+      <Toaster position="top-center" />
     </AuthProvider>
   </BrowserRouter>,
 );
